405192d3898efa5da7fba8b3e8918f1208a44c40
vorou <vorou@skbkontur.ru>
2014-05-05 23:23:33 +0200

    ws mock

---
+++ src/Grow.WebService/LineModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace Grow.WebService
+{
+    public class LineModule : NancyModule
+    {
+        public LineModule()
+        {
+            Post["/lines"] = _ => new {id = "123", name = "panda"};
+        }
+    }
+}
