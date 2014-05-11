263df8a065679f52fa88eecce34e94ac121bd712
vorou <vorou@skbkontur.ru>
2014-05-11 17:58:09 +0600

    fixed jshint warnings

--- src/Grow.WebApp/js/grow.js
+++ src/Grow.WebApp/js/grow.js
@@ -1,83 +1,82 @@
-$.ajaxPrefilter(function(options, originalOptions, jqXHR) {
-    options.url = 'http://localhost:7820' + options.url;
-});
+(function() {
+    'use strict';
+    $.ajaxPrefilter(function(options) {
+        options.url = 'http://localhost:7820' + options.url;
+    });
 
-var Line = Backbone.Model.extend({
-    urlRoot: '/lines'
-});
+    var Line = Backbone.Model.extend({
+        urlRoot: '/lines'
+    });
 
-var LineList = Backbone.Collection.extend({
-    model: Line,
-    url: '/lines'
-});
+    var LineList = Backbone.Collection.extend({
+        model: Line,
+        url: '/lines'
+    });
 
-var lineList = new LineList();
-lineList.fetch({
-    success: function(collection, response, options) {
-        console.log(collection);
-    }
-});
-lineList.on('add', function() {
-    dashboardView.render();
-});
+    var lineList = new LineList();
+    lineList.fetch();
+    lineList.on('add', function() {
+        dashboardView.render();
+    });
 
-var DashboardView = Backbone.View.extend({
-    el: '.dashboard',
+    var DashboardView = Backbone.View.extend({
+        el: '.dashboard',
 
-    initialize: function() {
-        this.$addButton = this.$el.find('button.add-line');
-        this.$newLineName = $('<input type="text" class="new-line-name form-control" placeholder="name" />');
-        this.$newLineName.hide();
-        this.$el.append(this.$newLineName);
+        initialize: function() {
+            this.$addButton = this.$el.find('button.add-line');
+            this.$newLineName = $('<input type="text" class="new-line-name form-control" placeholder="name" />');
+            this.$newLineName.hide();
+            this.$el.append(this.$newLineName);
 
-        this.$lines = $('<ul></ul>');
-        this.$el.prepend(this.$lines);
-    },
+            this.$lines = $('<ul></ul>');
+            this.$el.prepend(this.$lines);
+        },
 
-    render: function() {
-        var self = this;
-        self.$lines.empty();
-        lineList.forEach(function(line) {
-            var $line = $('<li>' + line.get('name') + '</li>');
-            self.$lines.append($line);
-        });
-    },
-
-    events: {
-        'click button.add-line': 'enterLineName',
-        'blur input': 'hideLineNameInput',
-        'keypress input': 'handleLineNameInputKeypress'
-    },
-    enterLineName: function() {
-        this.$addButton.hide();
-        this.$newLineName.show();
-        this.$newLineName.focus();
-    },
-    hideLineNameInput: function() {
-        this.$newLineName.hide();
-        this.$addButton.show();
-    },
-    handleLineNameInputKeypress: function(event) {
-        if (event.keyCode == 13) {
-            this.hideLineNameInput();
-            var line = new Line({
-                name: this.$newLineName.val()
+        render: function() {
+            var self = this;
+            self.$lines.empty();
+            lineList.forEach(function(line) {
+                var $line = $('<li>' + line.get('name') + '</li>');
+                self.$lines.append($line);
             });
-            lineList.create(line);
-            this.$newLineName.val('');
+        },
+
+        events: {
+            'click button.add-line': 'enterLineName',
+            'blur input': 'hideLineNameInput',
+            'keypress input': 'handleLineNameInputKeypress'
+        },
+        enterLineName: function() {
+            this.$addButton.hide();
+            this.$newLineName.show();
+            this.$newLineName.focus();
+        },
+        hideLineNameInput: function() {
+            this.$newLineName.hide();
+            this.$addButton.show();
+        },
+        handleLineNameInputKeypress: function(event) {
+            if (event.keyCode === 13) {
+                this.hideLineNameInput();
+                var line = new Line({
+                    name: this.$newLineName.val()
+                });
+                lineList.create(line);
+                this.$newLineName.val('');
+            }
         }
-    }
-});
+    });
 
-var dashboardView = new DashboardView();
+    var dashboardView = new DashboardView();
 
-var Router = Backbone.Router.extend({
-    routes: {
-        "": "home"
-    }
-});
-var router = new Router;
-router.on('route:home', function() {
-    dashboardView.render();
-})
-Backbone.history.start();
+    var Router = Backbone.Router.extend({
+        routes: {
+            "": "home"
+        }
+    });
+    var router = new Router();
+    router.on('route:home', function() {
+        dashboardView.render();
+    });
+    Backbone.history.start();
+}());
